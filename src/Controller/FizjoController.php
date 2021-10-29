9230b8df2f2a705b9d543f653196bf8f8bcdc5b1
KamilKamilK <k.kaczmarek@dotandspot.pl>
2021-10-29 20:40:58 +0200

    env configuration and front page

--- src/Controller/FizjoController.php
+++ src/Controller/FizjoController.php
@@ -3,14 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class FizjoController extends AbstractController
 {
     /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+
+    }
+    /**
      * @Route ("/", name="fizjo_index")
      */
-    public function index(){
-        return $this->render('base.html.twig');
+    public function index()
+    {
+        $html = $this->twig->render('index.html.twig');
+
+        return new Response($html);
     }
 }
