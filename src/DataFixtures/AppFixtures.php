09ea89f0f2cbdebc4210a5b2228ecc67c4a97f3c
KamilKamilK <k.kaczmarek@dotandspot.pl>
2021-12-29 21:47:39 +0100

    user entity created, message display improved

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,16 +3,35 @@
 namespace App\DataFixtures;
 
 use App\Entity\Fizjoterapy;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @param UserPasswordHasherInterface
+     */
+
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
+    {
+        $this->loadMethods($manager);
+        $this->loadUsers($manager);
+    }
+
+    public function loadMethods(ObjectManager $manager): void
     {
         $typeArray = ['fizykoterapia', 'kinezyterapia', 'masaż'];
 
-        for ($i=0; $i <10; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $fizjoterapy = new Fizjoterapy();
             $fizjoterapy->setHeader('Nazwa metody : ' . rand(0, 100));
             $fizjoterapy->setText('Opis metody : ' . rand(0, 100));
@@ -23,6 +42,20 @@ class AppFixtures extends Fixture
             $manager->persist($fizjoterapy);
         }
 
+        $manager->flush();
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('kamil_kkk');
+        $user->setFullname('Kamil KKK');
+        $user->setEmail('kamil@gmial.com');
+        $user->setPassword(
+            $this->passwordHasher->hashPassword(
+                $user, 'kamil123'));
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
